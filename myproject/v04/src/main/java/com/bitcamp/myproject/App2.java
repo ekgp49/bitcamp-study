7b1e9da4d2e2db2aee911ecfa41d469995072539
ekgp49 <ekgp4949@gmail.com>
2019-12-11 17:50:08 +0900

    v05

---
+++ myproject/v04/src/main/java/com/bitcamp/myproject/App2.java
@@ -0,0 +1,19 @@
+package com.bitcamp.myproject;
+
+public class App2 {
+
+  public static void main(String[] args) {
+    // 한 개의 카페 정보 출력
+    
+    System.out.println("상호 : 카페 0336");
+    System.out.println("주소 : 경기도 수원시 장안구 조원동");
+    System.out.println("연락처 : 010-5295-0336");
+    System.out.println("웹사이트 : www.cafe0336.com");
+    System.out.println("영업시간 : 6AM ~ 3PM");
+    System.out.println("정기휴일 : 격주 월요일");
+    System.out.println("메뉴 : 아메리카노");
+    System.out.println("별점 : 별5개");
+    
+  }
+
+}
---
+++ myproject/v05/src/main/java/com/bitcamp/myproject/App3.java
@@ -0,0 +1,51 @@
+package com.bitcamp.myproject;
+
+import java.sql.Date;
+import java.util.Scanner;
+public class App3 {
+
+  public static void main(String[] args) {
+    // 한 카페의 리뷰 출력
+    Scanner keyboard = new Scanner(System.in);
+    
+    Date createdDate = new Date(System.currentTimeMillis()); 
+    
+    
+    System.out.println("리뷰");
+    
+    
+    System.out.print("카페 상호는? : ");
+    String cafeName = keyboard.nextLine();
+    
+    System.out.print("고객은? : ");
+    String customer = keyboard.nextLine();
+    
+    System.out.print("별점은? : ");
+    String starRate = keyboard.nextLine();
+    
+    System.out.print("내용은? : ");
+    String content = keyboard.nextLine();
+    
+    int viewCount = 0;
+    
+    System.out.println();
+    
+    System.out.printf("상호 : %s\n", cafeName);
+    System.out.printf("고객 : %s\n", customer);
+    System.out.printf("별점 : %s\n", starRate);
+    System.out.printf("업로드 시간 : %s\n", createdDate);
+    System.out.printf("조회수 : %s\n", viewCount);
+    System.out.printf("내용 : %s\n", content);
+    
+    
+//    
+//    System.out.println("상호 : 카페 0336");
+//    System.out.println("고객 : 김다혜");
+//    System.out.println("별점 : 5개");
+//    System.out.println("업로드 시간 : 2019-12-11");
+//    System.out.println("내용 : 맛있어요");
+
+    keyboard.close();
+  }
+
+}
