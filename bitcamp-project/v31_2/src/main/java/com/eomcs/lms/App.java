1e99590f02b1d514db4adb1a9365ac598f497b99
ekgp49 <ekgp4949@gmail.com>
2020-02-03 19:03:01 +0900

    .

--- bitcamp-project/v31_2/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/v31_2/src/main/java/com/eomcs/lms/App.java
@@ -158,7 +158,6 @@ public class App {
   public static void main(String[] args) {
     App app = new App();
     app.addApplicationContextListener(new DataLoaderListener());
-    app.addApplicationContextListener(new GreetingListener());
 
     app.service();
   }
